f9939915a4290ac181ce55b128429fa0754191de
Carrie Howell <cbhsub@yahoo.com>
2021-08-14 16:00:46 -0700

    app changes

--- Helpers/AppRouteView.cs
+++ Helpers/AppRouteView.cs
@@ -21,7 +21,7 @@ namespace RecipeUi.Helpers
             if (authorize && AuthenticationService.User == null)
             {
                 var returnUrl = WebUtility.UrlEncode(new Uri(NavigationManager.Uri).PathAndQuery);
-                NavigationManager.NavigateTo($"login?returnUrl={returnUrl}");
+                NavigationManager.NavigateTo($"login");
             }
             else
             {
